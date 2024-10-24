950b549c82276bf41c866fe43548e12236049fc0
Ryan Smale <eagle14@gmail.com>
2024-10-25 12:27:17 +1300

    Wireframe StoryBoard layout of app flow, and ViewController stubs.

---
+++ BankLoanApplication/Application Forms/LoanDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoanDetailsViewController.swift
+//  BankLoanApplication
+//
+//  Created by Ryan Smale on 25/10/2024.
+//
+
+import UIKit
+
+class LoanDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
