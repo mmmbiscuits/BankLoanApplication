dc6db098e20f1b47a95b1ec5f7f87f96fabb1c0c
Ryan Smale <eagle14@gmail.com>
2024-10-26 10:50:21 +1300

    Storyboard Layout and design Improvements
    - Outlets to ViewControllers
    - Segues for navigating through app flow

--- BankLoanApplication/Application Forms/LoanApplicantDetailsViewController.swift
+++ BankLoanApplication/Application Forms/LoanApplicantDetailsViewController.swift
@@ -9,13 +9,24 @@ import UIKit
 
 class LoanApplicantDetailsViewController: UIViewController {
 
+    @IBOutlet weak var fullNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var genderTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+        
+    @IBOutlet weak var proceedToLoanDetailsButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.title = "Applicant Details"
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func goToLoanDetailsButtonPressed(_ sender: Any) {
+        self.performSegue(withIdentifier: "goToLoanDetailsSegueId", sender: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
--- BankLoanApplication/Application Forms/LoanDetailsViewController.swift
+++ BankLoanApplication/Application Forms/LoanDetailsViewController.swift
@@ -9,13 +9,27 @@ import UIKit
 
 class LoanDetailsViewController: UIViewController {
 
+    @IBOutlet weak var incomeTextField: UITextField!
+    @IBOutlet weak var loanAmountTextField: UITextField!
+    @IBOutlet weak var irdNumberTextField: UITextField!
+    
+    private let loanToIncomeRatio: Double = 0.5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.title = "Loan Details"
         // Do any additional setup after loading the view.
     }
     
 
+    @IBAction func goToLoanSummaryButtonPressed(_ sender: Any) {
+        
+        //if all are valid
+        
+        //else
+        
+        self.performSegue(withIdentifier: "loanApplicationSummarySegueID", sender: nil)
+    }
     /*
     // MARK: - Navigation
 
