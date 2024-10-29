acddc42915abfeb717da7ba472ab5a88e5fcc42b
Ryan Smale <eagle14@gmail.com>
2024-10-29 19:50:56 +1300

    putting segue id’s in a enum

---
+++ BankLoanApplication/Formatters/Conveniences/SegueIds.swift
@@ -0,0 +1,13 @@
+//
+//  StaticStrings.swift
+//  BankLoanApplication
+//
+//  Created by Ryan Smale on 29/10/2024.
+//
+
+enum SegueIdentifiers: String {
+    case unwindAndSaveSegueIdentifier = "saveAndUnwindToListSegueID"
+    case startNewLoanApplicationSegueIdentifier = "showAddLoanFlowSegueID"
+    case showLoanApplicationDetailsSegueIdentifier = "showLoanApplicationDetailsSegueID"
+    case showLoanDetailsSegueIdentifier = "loanApplicationSummarySegueID"
+}
